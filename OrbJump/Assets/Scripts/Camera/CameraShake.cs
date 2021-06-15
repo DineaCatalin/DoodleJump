cddbe1a03f5074d4012485d2314c1fcd10e09f26
unknown <catalindinea@yahoo.com>
2021-06-15 20:16:13 +0300

    Added camera shake on death

---
+++ OrbJump/Assets/Scripts/Camera/CameraShake.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+	[SerializeField] private Transform _cameraTransform;
+
+	[SerializeField] private float _shakeDuration = 0.5f;
+	[SerializeField] private float _shakeAmount = 0.7f;
+	[SerializeField] private float _decreaseFactor = 1.0f;
+	[SerializeField] private float _shakeRate = 0.05f;
+
+	private Vector3 _originalPos;
+	private float   _currentShakeDuration;
+
+	public void Shake()
+	{
+		_currentShakeDuration = _shakeDuration;
+		InvokeRepeating("DoShake", 0, _shakeRate);
+		_originalPos = _cameraTransform.localPosition;
+	}
+
+	private void DoShake()
+	{
+		if (_currentShakeDuration > 0)
+		{
+			_cameraTransform.position = _originalPos + Random.insideUnitSphere * _shakeAmount;
+
+			_currentShakeDuration -= Time.deltaTime * _decreaseFactor;
+		}
+		else
+		{
+			_currentShakeDuration = 0f;
+			_cameraTransform.position = _originalPos;
+			CancelInvoke("DoShake");
+		}
+	}
+}
